11cb31cef85b738abba656c6c92b579613ba2737
Luiz Fernando Soares Junior <lsoares-@c2r8s4.42lisboa.com>
2022-11-07 19:32:19 +0000

    trying to generate random senteces using words from "/usr/share/dict/words"

--- libft_Project/main.c
+++ libft_Project/main.c
@@ -4,15 +4,13 @@
 #include<stdlib.h>
 #include<time.h>
 #include<ctype.h>
+#include<err.h>
 #include"libft/libft.h"
 #include"libftest/libftest.h"
 
 #define N 10
 
-//void fei(int n, ...);
-void test_strlen(void);
-void rand_n_test(size_t bot);
-void memmove_tester(void);
+void fei(void);
 
 int main()
 {
@@ -25,17 +23,10 @@ int main()
 	test_ctype(N, ft_toupper, toupper, "toupper");
 	test_ctype(N, ft_tolower, tolower, "tolower");
 
-	//rand_n_test(3);
-	//test_strlen();
-	//fei(N, ft_memset);
-	memmove_tester();
-		
+//	memmove_tester();
+	fei();		
 	return (0);
 
-	/*
-	char *str = "This is a test phrase";
-	printf("%s\n", ft_substr(str, 5, 2));
-	*/
 }
 
 //uses libftest and ctype : random string with printible charcters
@@ -47,104 +38,43 @@ void random_str(char *s, size_t n)
 		s[i] = '\0';
 }
 
-/*void fei(int n, ...)
+char *fuu(char *buffer)
 {
-	va_list args;
-	int i = 0;
-
-	va_start(args, n);
-
-	while (i < n)
+	//Open dictionary of words
+	char *path = "/usr/share/dict/words";
+	FILE *word_stream = fopen(path, "r");
+	if (!word_stream)
+		err(1, "%s", path);
+	else
 	{
-		size_t len = rand_n(50, 3);
-
-		char ft_s[len];
-		char s[len];
-
-		random_str(ft_s, len);
-		strncpy(s, ft_s, len);
-		if (va_arg(args, void *(*()p_ft)(void*)(int)(size_t)))
-			unsigned int c = rand_u8bit('p');
-		size_t n_bytes = rand_n(len, 2) - 1;
-		size_t index_starter = rand_n(len - n_bytes, 0);
-		
-		printf("%d Str: %s Len: %lu\n", i, ft_s, strlen(ft_s));
-		printf("%d Str: %s Len: %lu\n", i, s, strlen(ft_s));
-		ft_memset(ft_s + index_starter, c, n_bytes * sizeof(char));
-		memset(s + index_starter, c, n_bytes * sizeof(char));
-		printf("c: %c - n_bytes: %lu - index_starter: %lu - s: %s\n", c, n_bytes, index_starter, ft_s);
-		printf("c: %c - n_bytes: %lu - index_starter: %lu - s: %s\n", c, n_bytes, index_starter, s);
-		i++;
-	}
-}*/
-
-
-void test_strlen(void)
-{
-	size_t i = 0;
-	size_t j = 0;
-	while (i < 20)
-	{
-		short len = (short)rand_n(30, 2);
-		char str[len];
-		random_str(str, len);
-		if ((len =! (short)(ft_strlen(str) + 1)))
+		//Get word from dictionary
+		char c;
+		size_t i_nl = 0;
+		size_t rand_w = rand() % 102401;
+		char *ptr_w = buffer;
+		while (fread(&c, sizeof(char), 1, word_stream) && i_nl <= rand_w)
 		{
-			printf("%d %s %ld KO!\n", len, str, ft_strlen(str));
-			j++;
+			if (c == '\n')
+			{
+				if (i_nl == rand_w)
+					*ptr_w = '\0';
+				i_nl++;
+			}
+			else if (i_nl == rand_w)
+				*ptr_w++ = c;
 		}
-		i++;
+		return (buffer);
 	}
-	if (!j)
-		printf("ft_strlen OK\n");
 }
 
-void rand_n_test(size_t bot)
+void fei(void)
 {
-	unsigned short i = 0;
-	unsigned short top = 40;
-	while (i < top)
+	int i = 0;
+	char word_buffer[45];
+	while (i <= 5)
 	{
-		printf("%2lu, ",  rand_n(top, bot));
+		printf("%s ", fuu(word_buffer));
 		i++;
 	}
 	putchar('\n');
 }
-
-void print_i_hex(size_t len)
-{
-	size_t i = 0;
-	while (len-- != 0)
-		printf("%.2lx,", i++);
-	printf("\b\n");
-}
-
-void print_str_1bit_spaces(char *str)
-{
-	char *ptr = str;
-	while(*ptr++)
-		printf(" %c ", *ptr);
-	putchar('\n');
-}
-
-void memmove_tester(void)
-{
-	int n = 10;
-	while(n-- != 0)
-	{
-		size_t len = rand_n(20, 3);
-		char str[len];
-		random_str(str, len);
-		size_t i_src = rand_n(len, 1);
-		size_t i_dest = rand_n(len, 1);
-		size_t n_bytes = rand_n(len, 1); 
-		
-		printf("len: %lu - i_src: %.2lx - i_dest: %.2lx - n_bytes: %lu\n", len, i_src, i_dest, n_bytes);
-		print_str_1bit_spaces(str);
-		print_i_hex(len - 1);
-		memmove(i_dest + str, i_src + str, n_bytes);
-		print_str_1bit_spaces(str);
-		putchar('\n');
-	}
-}
-
