6865ead04dba4c9b06e4f2ae3ff9156236fb197c
Luiz Fernando Soares Junior <lsoares-@c2r1s8.42lisboa.com>
2022-11-10 19:26:33 +0000

    tester for mem functions working with memcpy and memmove, bug to fix in ft_memcpy

--- libft_Project/libft/ft_memcpy.c
+++ libft_Project/libft/ft_memcpy.c
@@ -1,10 +1,11 @@
 #include<stdlib.h>
 
-void	*memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char *ptr = dest;
-	while (n--)
-		*(char*)ptr++ = *(const char*)src++;
+	unsigned char *ptr = dest;
+	size_t i = 0;
+	while (i < n)
+		ptr[i++] = *(unsigned char*)src++;
 	return (dest);
 
 }
--- libft_Project/main.c
+++ libft_Project/main.c
@@ -8,10 +8,13 @@
 #include"libft/libft.h"
 #include"libftest/libftest.h"
 
-#define N_TEST 10
-#define N 3
+#define N_TEST 5
+#define N 5
 #define WORD 45
 
+typedef void *(*memFunction)(void *, const void *, size_t);
+
+void test_mem(size_t n, memFunction p_f, memFunction p_ft, char *f_name);
 char *random_sentence(size_t n, char* buffer);
 void mesure(size_t n);
 
@@ -26,41 +29,8 @@ int main()
 	test_ctype(N_TEST, ft_toupper, toupper, "toupper");
 	test_ctype(N_TEST, ft_tolower, tolower, "tolower");
 	
-	//Definig the buffer the size and initializing it
-	size_t size = N * WORD;
-	char buffer[size];
-	bzero(buffer, size);
-
-	size_t i = 0;
-
-	while (i < N_TEST)
-	{
-		size_t dest = rand_n(size, 1);
-		size_t src = rand_n(size, 1);
-		size_t n_len = rand_n(size, 1);
-
-		//Filling the buffer with a random string of words
-		random_sentence(N, buffer);
-
-		//Defining the test buffer and copying the buffer to it
-		char buffer_ft[size];
-		memcpy(buffer_ft, buffer, size);
-		
-		//Printing the buffer before and after memmove
-		printf("Buffer:     %s\n", buffer);
-		memmove(buffer + dest, buffer + src, n_len);
-		printf("Memmove:    %s\n", buffer);
-
-		//Printing the buffer before and after ft_memmove
-		printf("Buffer:     %s\n", buffer_ft);
-		ft_memmove(buffer_ft + dest, buffer_ft + src, n_len);
-		printf("Ft_memmove: %s\n", buffer_ft);
-
-		putchar('\n');
-
-		i++;
-	}
-
+	//test_mem(N_TEST, memmove, ft_memmove, "memmove");
+	test_mem(N_TEST, memcpy, ft_memcpy, "memcpy");
 	
 
 	return (0);
@@ -126,4 +96,50 @@ void mesure(size_t n)
 	while(i < n)
 		printf("%ld", (i++) % 10);
 	putchar('\n');
+}
+//uses string.h, stdio.h.
+void test_mem(size_t n, memFunction p_f, memFunction p_ft, char *f_name)
+{
+	//Definig the buffer the size and initializing it
+	size_t size = N * WORD;
+	char buffer[size];
+	bzero(buffer, size);
+
+	size_t i = 0;
+
+	while (i < n)
+	{
+		//Filling the buffer with a random string of words
+		random_sentence(N, buffer);
+
+		//Defining the test buffer and copying the buffer to it
+		char buffer_ft[size];
+		memcpy(buffer_ft, buffer, size);
+		
+		//Defining random values for testing
+		size_t n_len = rand_n(size, 1);
+		size_t dest = rand_n(size - n_len, 1);
+		size_t src = rand_n(size - n_len, 1);
+
+		//Printing the test values
+		printf("            dest:%lu src:%lu len:%lu\n", dest, src, n_len);
+		//Printing the buffer before and after p_f
+		printf("buffer:     %s\n", buffer);
+		p_f(buffer + dest, buffer + src, n_len);
+		printf("%s:     %s\n",f_name, buffer);
+
+		//Printing the buffer before and after p_ft
+		p_ft(buffer_ft + dest, buffer_ft + src, n_len);
+		printf("ft_%s:  %s\n", f_name, buffer_ft);
+	
+		//Verify if both buffers have the same value and prints
+		if (memcmp(buffer, buffer_ft, size))
+			printf("KO\n");
+		else
+			printf("OK\n");
+		putchar('\n');
+
+		i++;
+	}
+
 }
