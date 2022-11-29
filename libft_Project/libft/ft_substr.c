fe2466951bae814c1457ae57ba0e7a7748e23f1b
LuizSoaresJr <luizsoaresjr@ymail.com>
2022-11-29 13:22:58 +0000

    .

--- libft_Project/libft/ft_substr.c
+++ libft_Project/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lsoares- <lsoares-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 17:22:12 by lsoares-          #+#    #+#             */
-/*   Updated: 2022/11/28 18:00:08 by lsoares-         ###   ########.fr       */
+/*   Updated: 2022/11/29 13:20:39 by lsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,34 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	real_len;
 	char		*ptr;
 
-	real_len = ft_strlen(s) - start;
-	if (len > real_len)
-		ptr = malloc(real_len + 1);
+	if (len == 1 || start > ft_strlen(s))
+	{
+		ptr = malloc(1);
+		if (!ptr)
+			return (NULL);
+		ft_bzero(ptr, 1);
+	}
 	else
+	{
 		ptr = malloc(len + 1);
-	if (!ptr)
-		return (NULL);
-	ft_strlcpy(ptr, s + start, len + 1);
+		if (!ptr)
+			return (NULL);
+		ft_strlcpy(ptr, s + start, len + 1);
+	}
 	return (ptr);
 }
-
 /*
 int main (void)
 {
 	char *s = "Eu sou inteligente.";
-	char *resut = ft_substr(s, 7, 0);
+	char *resut = ft_substr("", 0, 0);
 	printf("%s\n", resut);
 }
 */
+
+//len = 1 return "";
+//		malloc(1);
+// start > sizeof(len) return "";
+//		malloc(1);
